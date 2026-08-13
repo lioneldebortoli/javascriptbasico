cc5de6766b023e4e667d9ced809a8bc8d9b33841
lionelDebortoli <lionelfacultad123@gmail.com>
2026-08-13 20:39:16 -0300

    feat: agrega variables y tipos de datos

--- js/01-variables.js
+++ js/01-variables.js
@@ -1,13 +1,23 @@
-//esto es un comentario de una linea
-
-/* comentario multiliea */
-
-let precioCompra;
-
-let descuento;
-
-let nombre;
-
-nombre = "Alejandro";
 
 
+// alert("Bienvenido")
+
+// const nombreUsuario = prompt("Ingresa tu nombre");
+ 
+//console.log(nombreUsuario);
+
+// document.writeln(`Bienvenido ${nombreUsuario}`)
+
+// let num1 = Number(prompt("Ingrese un numero"))
+
+// console.log(numb1);
+
+let numUno = parseInt(prompt("Ingrese un numero "));
+
+let numDos = parseInt(prompt("Ingrese otro numero")) ;
+
+let suma;
+
+suma = numUno + numDos;
+
+document.writeln(`El valor de la suma de los numeros ingresados es ${suma}`);
